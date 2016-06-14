4b32f82e6d63a8b622b42ed1d536cd2c63b0610a
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-06-14 11:24:23 -0400

    Added 1.0 update things

---
+++ MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by Randi Kaminsky on 6/14/16.
+//  Copyright © 2016 Usernamer. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
